d0983dcf7b4a49da00d18fb7bbba180deaf4db1e
Albin Sunnanbo <albin@sunnanbo.com>
2015-10-04 20:46:28 +0200

    Add a minimalistic code first database to support simple examples

---
+++ MvcDemo/Entities/Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcDemo.Entities
+{
+    public class Context : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ MvcDemo/Migrations/201510041844574_Initial.cs
@@ -0,0 +1,27 @@
+namespace MvcDemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        CustomerId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Age = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.CustomerId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+        }
+    }
+}
