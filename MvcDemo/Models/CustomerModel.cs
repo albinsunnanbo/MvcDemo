6b17d32a827aac6dfb2d94e185d08e55b48eab46
Albin Sunnanbo <albin@sunnanbo.com>
2015-10-04 21:04:09 +0200

    Labels and display name

--- MvcDemo/Models/CustomerModel.cs
+++ MvcDemo/Models/CustomerModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +11,7 @@ namespace MvcDemo.Models
     {
         public int CustomerId { get; set; }
 
+        [Display(Name = "Kundnamn")]
         public string CustomerName { get; set; }
     }
 }
