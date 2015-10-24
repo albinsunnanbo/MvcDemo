d29c4b77f52d2f1b72e7d8285551dd00434cdfc5
Albin Sunnanbo <albin@sunnanbo.com>
2015-10-24 20:15:21 +0200

    Display ajax loading

--- MvcDemo/Scripts/Views/DropDown.js
+++ MvcDemo/Scripts/Views/DropDown.js
@@ -3,6 +3,7 @@
 
     $("#County").change(function () {
         var placeholder = $("#municipality-placeholder");
+        placeholder.html("Laddar...");
         var url = placeholder.data("url");
         $.post(url, $(this).closest("form").serialize(), function (data, textStatus, jqXHR) {
             // When server returns ajax response, replace the placeholder content with the new dropdown
